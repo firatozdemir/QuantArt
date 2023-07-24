fec8da9c0a301dd56d0a66e0a7fc0cd60cf5b3fe
Firat <fozdemir@gmail.com>
2023-07-24 14:10:42 +0000

    bugfix

---
+++ train_oadat_a100.sh
@@ -0,0 +1,17 @@
+# export PATH=/opt/conda/bin:$PATH
+# sudo mount -o remount,size=160G /dev/shm
+
+# # Stage-1.1
+python -u main.py \
+--base configs/vqgan_oadat_sim.yaml \
+--train True --gpus 0,1
+
+# Stage-1.2
+# python -u main.py \
+# --base configs/vqgan_oadat_swfd.yaml \
+# --train True --gpus 0,1
+
+# Stage-2
+# python -u main.py \
+# --base configs/oadat_sim2invivo.yaml \
+# --train True --gpus 0,1
