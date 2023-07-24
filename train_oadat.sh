f460b0bad0ea82cf097d6c9d628b71b273cf2958
Firat <fozdemir@gmail.com>
2023-07-24 13:52:07 +0000

    .

--- train_oadat.sh
+++ train_oadat.sh
@@ -1,12 +1,14 @@
+# export PATH=/opt/conda/bin:$PATH
+
 # # Stage-1.1
-python -u main.py \
---base configs/vqgan_oadat_sim.yaml \
---train True --gpus 0,1
+# python -u main.py \
+# --base configs/vqgan_oadat_sim.yaml \
+# --train True --gpus 0,1
 
 # Stage-1.2
 python -u main.py \
 --base configs/vqgan_oadat_swfd.yaml \
---train True --gpus 0,1
+--train True --gpus 0,
 
 # Stage-2
 # python -u main.py \
